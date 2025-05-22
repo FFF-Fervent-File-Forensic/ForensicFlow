7713c079c8fc9df6afdb353ac717c1b662674ac9
pphy03 <djbsjse22@naver.com>
2025-05-22 17:46:20 +0900

    feat: 리액트 프로젝트 초기 세팅 및 구조 생성

---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <p> 리액트 적용 확인 </p>
+    </div>
+  );
+}
+
+export default Main;
