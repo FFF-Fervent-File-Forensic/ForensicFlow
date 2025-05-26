b8f786fc92ee7df9ee04bee4070edaa53d73673b
danger10043 <danger1004372@gmail.com>
2025-05-26 19:49:51 +0900

    Push 테스트

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Main() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <p> 리액트 적용 확인 </p>
+      <p> Push Test </p>
     </div>
   );
 }
